f73e51cc88fbeb355663248d42966137a86a695f
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2025-09-21 02:43:33 -0400

    Add initial project structure with Clerk integration and UI components
    - Updated .gitignore to include Clerk configuration.
    - Added components.json for component configuration.
    - Implemented middleware for protected routes using Clerk.
    - Enhanced next.config.ts to support remote images.
    - Introduced various UI components including Accordion, AlertDialog, Avatar, and more.
    - Created pages for login, dashboard, and main application layout.
    - Added utility functions and hooks for mobile responsiveness.
    - Established global styles and layout structure for the application.

---
+++ app/app/page.tsx
@@ -0,0 +1,3 @@
+export default function dashboard() {
+  return <h1>App route. Should be redirected to dashboard</h1>;
+}
---
+++ components/web/Hero.tsx
@@ -0,0 +1,98 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+
+export interface HeroProps {
+  title: string;
+  description: string;
+  primaryButtonText: string;
+  primaryButtonLink: string;
+  secondaryButtonText: string;
+  secondaryButtonLink: string;
+  imageUrl: string;
+}
+
+export const Hero = ({
+  title,
+  description,
+  primaryButtonText,
+  primaryButtonLink,
+  secondaryButtonText,
+  secondaryButtonLink,
+  imageUrl,
+}: HeroProps) => {
+  const getButton = (
+    text: string,
+    link: string,
+    variant: "primary" | "secondary",
+  ) => {
+    const buttonVariant = variant === "primary" ? "default" : "outline";
+    return link.startsWith("/signin") ? (
+      <Button
+        className="w-full sm:w-auto"
+        size="lg"
+        asChild
+        variant={buttonVariant}
+      >
+        <SignInButton fallbackRedirectUrl="/app/dashboard">{text}</SignInButton>
+      </Button>
+    ) : link.startsWith("/signup") ? (
+      <Button
+        className="w-full sm:w-auto"
+        size="lg"
+        asChild
+        variant={buttonVariant}
+      >
+        <SignUpButton fallbackRedirectUrl="/">{text}</SignUpButton>
+      </Button>
+    ) : (
+      <Button
+        className="w-full sm:w-auto"
+        size="lg"
+        asChild
+        variant={buttonVariant}
+      >
+        <Link href={link}>{text}</Link>
+      </Button>
+    );
+  };
+  const secondaryButton = getButton(
+    secondaryButtonText,
+    secondaryButtonLink,
+    "secondary",
+  );
+  const primaryButton = getButton(
+    primaryButtonText,
+    primaryButtonLink,
+    "primary",
+  );
+  return (
+    <section className="overflow-hidden py-32">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between gap-20 lg:flex-row">
+          <div className="flex flex-col items-center gap-6 text-center lg:items-start lg:text-left">
+            <h1 className="text-4xl font-bold text-pretty lg:max-w-md lg:text-7xl">
+              {title}
+            </h1>
+            <p className="max-w-xl text-xl font-medium lg:text-2xl">
+              {description}
+            </p>
+            <div className="flex w-full justify-center lg:justify-start gap-4">
+              {primaryButton}
+              {secondaryButton}
+            </div>
+          </div>
+          <Image
+            src={imageUrl}
+            alt="placeholder hero"
+            className="aspect-video rounded-md object-cover"
+            width={600}
+            height={400}
+            unoptimized
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
